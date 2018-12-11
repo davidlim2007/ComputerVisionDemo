60abb8236201f8c784bd88c8fae60bebcf17e9b5
David Lim <beitrik2014@gmail.com>
2018-12-11 15:56:34 +0800

    Added comments in code and changed Window layout

--- ComputerVisionDemo/MainWindow.xaml.cs
+++ ComputerVisionDemo/MainWindow.xaml.cs
@@ -1,19 +1,14 @@
 ﻿using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
 using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
-
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
 namespace ComputerVisionDemo
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         // The ComputerVisionClient object used to perform calls to the
@@ -27,6 +22,8 @@ namespace ComputerVisionDemo
         // The local filepath of the image being uploaded to the API.
         private string filePath;
 
+        // A List that specifies the features to return from the Computer
+        // Vision API. This only applies to Image Analysis.
         private static readonly List<VisualFeatureTypes> features =
             new List<VisualFeatureTypes>()
         {
@@ -35,6 +32,8 @@ namespace ComputerVisionDemo
             VisualFeatureTypes.Tags
         };
 
+        // The Text Recognition type to help the Computer Vision API recognize
+        // and extract text from an image. Can be toggled between Printed and Handwritten.
         private const TextRecognitionMode textRecognitionMode = TextRecognitionMode.Printed;
         private const int numberOfCharsInOperationId = 36;
 
@@ -43,8 +42,16 @@ namespace ComputerVisionDemo
             InitializeComponent();
         }
 
+        // MSCognitiveServicesLogin_Click() is the handler for the Click event of the
+        // button named MSCognitiveServicesLogin in the xaml.
+        //
+        // This handler responds by instantiating a new ComputerVisionClient object 
+        // based on the Computer Vision API key supplied in the CVKey TextBox.
         private void MSCognitiveServicesLogin_Click(object sender, RoutedEventArgs e)
         {
+            // If the user has not entered a key in the CVKey textbox,
+            // display an error message prompting the user to enter the
+            // key.
             if (String.IsNullOrEmpty(CVKey.Text))
             {
                 lblError.Content = "Please enter a Computer Vision API Key.";
@@ -53,20 +60,35 @@ namespace ComputerVisionDemo
             }
             
             lblError.Visibility = Visibility.Hidden;
+
+            // Else, instantiate computerVision using the entered key.
             computerVision = new ComputerVisionClient(
                 new ApiKeyServiceClientCredentials(CVKey.Text),
                 new System.Net.Http.DelegatingHandler[] { }
             );
 
+            // If computerVision has been successfully instantiated, the connection
+            // to the Computer Vision API is deemed successful. We display a success
+            // message and disable the CVKey textbox and MSCognitiveServicesLogin button.
+            //
+            // Else, if computerVision could not be instantiated, we display an error
+            // message.
             if (computerVision != null)
             {
-                CVKey.Text = "";
+                CVKey.Text = "Connection successful.";
+                CVKey.IsReadOnly = true;
                 MSCognitiveServicesLogin.IsEnabled = false;
                 computerVision.Endpoint = "https://westcentralus.api.cognitive.microsoft.com";
-                lblError.Content = "Connection successful.";
+            }
+            else
+            {
+                lblError.Content = "Connection failed, please try again!";
+                lblError.Visibility = Visibility.Visible;
             }
         }
 
+        // BrowseButton_Click() is the event handler shared by the "Browse" button.
+        // 
         private void BrowseButton_Click(object sender, RoutedEventArgs e)
         {
             // Get the image file to scan from the user.
